1194855d1f984d04ed976b32e8fee5b8baa7b236
Barbos20 <92588487+Barbos20@users.noreply.github.com>
2024-11-27 18:26:35 +0300

    feat: Добавление изображений и шрифтов. (#1)

---
+++ lib/common/app_text_style.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const inter500 = TextStyle(
+    fontFamily: 'inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const montserrat500 = TextStyle(
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const montserrat600 = TextStyle(
+    fontFamily: 'montserrat',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    color: Colors.white,
+  );
+
+  static const raleway700 = TextStyle(
+    fontFamily: 'raleway',
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    color: Colors.white,
+  );
+
+  static const raleway500 = TextStyle(
+    fontFamily: 'raleway',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const velaSans600 = TextStyle(
+    fontFamily: 'vela-sans',
+    fontWeight: FontWeight.w600,
+    fontSize: 22,
+    color: Colors.white,
+  );
+
+  static const velaSans400 = TextStyle(
+    fontFamily: 'vela-sans',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static TextStyle customStyle({
+    required TextStyle baseStyle,
+    double? fontSize,
+    Color? color,
+    FontWeight? fontWeight,
+  }) {
+    return baseStyle.copyWith(
+      fontSize: fontSize,
+      color: color,
+      fontWeight: fontWeight,
+    );
+  }
+}
