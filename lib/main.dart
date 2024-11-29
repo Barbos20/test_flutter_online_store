b81aac792bbd2b38da4324d06707f3d30dff3302
Barbos20 <92588487+Barbos20@users.noreply.github.com>
2024-11-29 13:24:22 +0300

    feat: Создание offeredWidget. (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:test_online_store/utils/all_text.dart';
+import 'package:test_online_store/features/basket_screen/basket_screen.dart';
+import 'package:test_online_store/features/catalog_screen/catalog_screen.dart';
+import 'package:test_online_store/features/main_screen/main_screen.dart';
+import 'package:test_online_store/features/profile_screen/profile_screen.dart';
+import 'package:test_online_store/utils/app_text_style.dart';
+import 'package:test_online_store/utils/assets_pictures.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const MainScreen(),
+    const CatalogScreen(),
+    const BasketScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const labelStyle = AppTextStyles.raleway500;
+    const selectedColor = Colors.black;
+
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(),
+      home: Scaffold(
+        body: _screens[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: selectedColor,
+          unselectedLabelStyle: labelStyle,
+          selectedLabelStyle: labelStyle.copyWith(
+            color: selectedColor,
+          ),
+          items: const [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AssetsPictures.house),
+              ),
+              label: AllText.main,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AssetsPictures.magnifyingGlass),
+              ),
+              label: AllText.catalog,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AssetsPictures.shoppingCartSimple),
+              ),
+              label: AllText.basket,
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AssetsPictures.user),
+              ),
+              label: AllText.profile,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
